e8cdf6ef0a0328a82827b30d5fe6f3f22ca6ae3c
StephOg <stephanieogwo@Stephanies-MBP.lan>
2024-07-15 09:44:40 -0400

    added middleware

--- app.js
+++ app.js
@@ -4,17 +4,30 @@ const port = 3030;
 
 const fs = require('fs'); //import filesystem to read template views (not sure if I will need with Pug)
 const path = require('path'); // Using Path
+const cookieParser = require('cookie-parser');
+
 
 //set up Pug
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+//Middleware
+app.use(cookieParser());
+
 // Define a route for the root URL
 app.get('/', (req, res) => {
-    res.render("login",); //will fill this with more info
+    res.render("login",);
+    console.log('Cookies: ', req.cookies);
 });
 app.get("/signup", (req, res) => {
-    res.render("signup",) //will fill this with more info
+    res.render("signup",)
+});
+
+//error handler
+
+
+app.use((err, req, res, next) => {
+    res.status(400).send(err.message);
 });
 
 // Start the server
